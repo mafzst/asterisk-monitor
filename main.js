7d710aa0f323bb303930d17c5169939f3f9e8d95
Nicolas Perraut <n.perraut@gmail.com>
2017-09-27 21:39:55 +0000

    Move to GitHub

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = express()
 /* Serve VueJs app (in public/dist) on the default route */
 app.use(express.static(__dirname + '/public/dist'))
 
-var port = process.env.PORT || 5000;
-app.listen(port);
+var port = process.env.PORT || 5000
+app.listen(port)
 
 console.log('Server started on port ' + port)
