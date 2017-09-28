074c364db6279537290d3b6635262a7916909272
Nicolas Perraut <n.perraut@gmail.com>
2017-09-28 03:37:31 +0000

    Simplifying config

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  asterisk: {
-    host: '192.168.1.11',
-    port: 5038,
-    username: 'hello',
-    password: 'world'
-  },
-  amiEndpoints: ['ping', 'sippeers']
-}
--- config/index.js
+++ config/index.js
@@ -1,4 +1,9 @@
-const prod = require('./prod.js')
-const dev = require('./dev.js')
-
-module.exports = Object.assign({}, prod, dev)
+module.exports = {
+  asterisk: {
+    host: process.env.ASTERISK_HOST || '192.168.1.11',
+    port: process.env.ASTERISK_PORT || 5038,
+    username: process.env.ASTERISK_USERNAME || 'hello',
+    password: process.env.ASTERISK_PASSWORD || 'world'
+  },
+  amiEndpoints: ['ping', 'sippeers']
+}
