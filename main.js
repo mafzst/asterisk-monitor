93b587c343a5db748b63d11dc6c9a668af0e4867
Nicolas Perraut <n.perraut@gmail.com>
2017-09-30 21:25:40 +0000

    Fix required unused module

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-require('express-ws')(app)
 
 const config = require('./config/index')
 const Asterisk = require('./classes/asterisk')
